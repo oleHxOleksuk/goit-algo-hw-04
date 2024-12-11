afcf8c9c9781e790f7cc8800e5ca07f7c1c5d594
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2024-12-11 22:14:19 +0200

    task-4

---
+++ Task_4/task_4.py
@@ -0,0 +1,47 @@
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added."
+
+def show_all(contacts:dict):
+    if contacts:
+        return contacts
+    else:
+        return "Is not contacts"
+def show_phone(args:list, contacts:dict):
+    name = args[0]
+    if name not in contacts:
+        return "Contact does not exist"
+    else:
+        return contacts[name]
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
+
