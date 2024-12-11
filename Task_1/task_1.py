c5eca8701221c2d4470591eef96e879b80d1a95b
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2024-12-11 19:13:32 +0200

    Task-1

---
+++ Task_1/task_1.py
@@ -0,0 +1,29 @@
+path = './Task_1/for_task_1.txt'
+
+def total_salary(path):
+    total = 0
+    count = 0
+    try:
+        with open(path, "r") as salary:
+            for num in salary:
+                all_num = num.strip().split(",")
+                if len(all_num) == 2:
+                    try:
+                        total += int(all_num[1])
+                        count += 1
+                    except ValueError:
+                        return print("Incorrect salary format")
+                else:
+                    return print('Incorrect data format')
+        if count > 0:
+            average = total/count
+            return total, average
+        else:
+            return print('There is no salary data')
+    except FileNotFoundError:
+        return print('file not found')
+            
+
+total, average = total_salary("./Task_1/for_task_1.txt")
+print(
+    f"Загальна сума заробітної плати: {int(total)}, Середня заробітна плата: {int(average)}")
